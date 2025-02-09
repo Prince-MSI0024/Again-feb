a51c289b447e49f0fa984bf7562c6a0a1df307a8
Prince-MSI0024 <happylap2k23@gmail.com>
2025-02-09 10:45:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ form.addEventListener('submit', (e) => {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
-		resultDiv.innerHTML = 'Naa tue amr Boobi, na kothy amr boobi';
+		resultDiv.innerHTML = 'Naa tue amr Boobi na, kothy amr boobi?';
 	}
 });
